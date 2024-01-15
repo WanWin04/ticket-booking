db4d6e59493003d2425866a96f36bfb2649183f3
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-01-15 21:27:31 +0700

    client - Fix Header.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "@material-ui/core";
 
-import Header from '../components/Header/index.HomePage.js';
+import Header from '../components/Header/index.Header.js';
 import PostList from "../components/PostList/index.PostList.js";
 
 export default function HomePage() {
